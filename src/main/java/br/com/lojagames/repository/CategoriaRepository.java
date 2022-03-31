9a4c3a23512337fbf95eec695bd871f60cc1e8a3
Matheus <matheusrabelosantos@hotmail.com>
2022-03-31 10:11:19 -0300

    Subindo Loja de Games

---
+++ src/main/java/br/com/lojagames/repository/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package br.com.lojagames.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.lojagames.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+	
+	public List<Categoria> findAllByTipoContainingIgnoreCase(String tipo);
+
+}
---
+++ src/main/java/br/com/lojagames/repository/ProdutoRepository.java
@@ -0,0 +1,17 @@
+package br.com.lojagames.repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.com.lojagames.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+	
+	public List<Produto> findAllByNomeContainingIgnoreCase(String nome);
+
+	public List <Produto> findByPrecoGreaterThanOrderByPreco(BigDecimal preco);
+
+	public List <Produto> findByPrecoLessThanOrderByPrecoDesc(BigDecimal preco);
+	
+}
